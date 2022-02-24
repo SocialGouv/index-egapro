df9bf29fc661e4e0dbb0de742f9063924c971ea7
pom421 <mauguet.po@gmail.com>
2022-02-24 17:12:21 +0100

    feat: add basePath in Next configuration

--- components/ds/Logo.tsx
+++ components/ds/Logo.tsx
@@ -6,7 +6,7 @@ export type LogoProps = Record<string, unknown>
 const Logo: FunctionComponent<LogoProps> = () => {
   return (
     <Image
-      src={"/icons/marianne.svg"}
+      src={"/consulter-index/icons/marianne.svg"}
       alt="Aller à la page d'accueil du Ministère du travail, de l'emploi et de l'insertion"
       width={88}
     />
--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: "/consulter-index",
 }
 
 module.exports = nextConfig
