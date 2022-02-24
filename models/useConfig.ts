7dbc0ca9d2345697b2ea07af3b0f0caa59030d77
pom421 <mauguet.po@gmail.com>
2022-02-24 13:29:39 +0100

    refactor: PUBLIC_YEARS used

--- models/useConfig.ts
+++ models/useConfig.ts
@@ -6,7 +6,7 @@ import React from "react"
 
 export type ConfigTypeApi = {
   YEARS: number[]
-  CURRENT_PUBLIC_YEAR: number
+  PUBLIC_YEARS: number[]
   EFFECTIFS: Record<string, string>
   DEPARTEMENTS: Record<string, string>
   REGIONS: Record<string, string>
@@ -20,6 +20,7 @@ export type ConfigTypeFormatted = ConfigTypeApi & {
   DEPARTEMENTS_TRIES: [string, string][]
   REGIONS_TRIES: [string, string][]
   SECTIONS_NAF_TRIES: [string, string][]
+  LAST_PUBLIC_YEAR: string
 }
 
 type SelectItemsType = [string, string][]
@@ -47,13 +48,14 @@ export function useConfig(): FetcherReturnImmutable & { data: ConfigTypeFormatte
   const isLoading = !data && !error
   const isError = Boolean(error)
 
-  const { REGIONS, DEPARTEMENTS, SECTIONS_NAF }: Partial<ConfigTypeApi> = data || {}
+  const { PUBLIC_YEARS, REGIONS, DEPARTEMENTS, SECTIONS_NAF }: Partial<ConfigTypeApi> = data || {}
 
   // Dénormalisation des données avec tri.
   const addon = {
     DEPARTEMENTS_TRIES: !DEPARTEMENTS ? [] : Object.entries(DEPARTEMENTS).sort((a, b) => a[1].localeCompare(b[1])),
     REGIONS_TRIES: !REGIONS ? [] : Object.entries(REGIONS).sort((a, b) => a[1].localeCompare(b[1])),
     SECTIONS_NAF_TRIES: !SECTIONS_NAF ? [] : Object.entries(SECTIONS_NAF).sort((a, b) => a[1].localeCompare(b[1])),
+    LAST_PUBLIC_YEAR: String(PUBLIC_YEARS?.sort()?.reverse()?.[0] || ""),
   }
 
   // We want to ensure that the data is always the same object on every render, once there is a value.
