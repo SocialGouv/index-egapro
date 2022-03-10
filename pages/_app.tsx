e16027fdcf456a145a78546abb7839d2421b52eb
pom421 <mauguet.po@gmail.com>
2022-03-10 12:21:38 +0100

    feat: add matomo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import "@fontsource/gabriela"
 import "@fontsource/cabin"
 
+import React from "react"
+import { ChakraProvider } from "@chakra-ui/react"
+import { init } from "@socialgouv/matomo-next"
+
 import type { ReactElement, ReactNode } from "react"
 import type { NextPage } from "next"
 import type { AppProps } from "next/app"
-import { ChakraProvider } from "@chakra-ui/react"
 
 import theme from "../theme"
 
@@ -18,7 +21,15 @@ type AppPropsWithLayout = AppProps & {
 }
 
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  React.useEffect(() => {
+    init({
+      url: process.env.NEXT_PUBLIC_MATOMO_URL ?? "",
+      siteId: process.env.NEXT_PUBLIC_MATOMO_SITE_ID ?? "",
+    })
+  }, [])
+
   // Use the layout defined at the page level, if available
+
   const getLayout = Component.getLayout ?? ((page) => page)
 
   const componentWithLayout = getLayout(<Component {...pageProps} />)
