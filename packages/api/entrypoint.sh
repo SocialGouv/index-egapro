df886759423eba05ddf43370200cb170021c4273
pom421 <mauguet.po@gmail.com>
2022-06-15 18:16:26 +0200

    feat: update for declaration app

--- packages/api/entrypoint.sh
+++ packages/api/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+set -e
+
+[ ! -d /app/egapro.egg-info ] && ln -s /egapro.egg-info /app/egapro.egg-info
+
 ./init.py
 
 exec gunicorn egapro.views:app -b 0.0.0.0:2626 --access-logfile=- --log-file=- --timeout 600 --worker-class roll.worker.Worker
